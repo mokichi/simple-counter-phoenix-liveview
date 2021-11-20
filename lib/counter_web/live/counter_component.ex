a8b0e27f214e7e56ae28f756a172e621a91d47c7
SAITO Kazuya <saito@smartalgorithm.co.jp>
2021-11-20 15:23:58 +0900

    Use Phoenix.LiveComponent

---
+++ lib/counter_web/live/counter_component.ex
@@ -0,0 +1,31 @@
+defmodule CounterWeb.CounterComponent do
+  use CounterWeb, :live_component
+
+  #def mount(socket) do
+  #  {:ok, socket}
+  #end
+
+  def update(assigns, socket) do
+    socket =
+      if Map.has_key?(assigns, :count) do
+        socket
+      else
+        assign(socket, :count, assigns.initial_count)
+      end
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div>
+      Count: <%= @count %>
+      <button phx-click="inc" phx-target={@myself}>+</button>
+    </div>
+    """
+  end
+
+  def handle_event("inc", _value, socket) do
+    new_count = socket.assigns.count + 1
+    {:noreply, assign(socket, :count, new_count)}
+  end
+end
